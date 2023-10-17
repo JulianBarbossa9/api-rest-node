c134a042b91007d2e109d0e2c9abfd941cb28222
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-17 17:27:07 -0500

    call controller into the item route

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,12 +1,18 @@
 import { Request, Response, Router } from "express";
+import { deleteItem, getItem, getItems, postItem, updateItem } from "../controllers/item";
 
 const router = Router()//This handle the routes, create a CRUD, req, res
 
 //Create a route http://localhost:3000/items
-router.get('/', (req: Request, res: Response) => {
-  res.send({ data: 'HERE_GOES_THE_DATA'})
-})
+router.get('/:id', getItem)
 
+router.get('/', getItems)
+
+router.patch('/:id', updateItem)
+
+router.post('/', postItem)
+
+router.delete('/:id', deleteItem)
 
 
 
