849c1ee568da86e268a2dfe413cea8463eb5c753
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-18 22:29:02 -0500

    call logMiddelware that use in getDetailItem

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from "express";
 import { deleteItem, getItem, getItems, postItem, updateItem } from "../controllers/item";
+import { logMiddelware } from "../middelware/log";
 
 const router = Router()//This handle the routes, create a CRUD, req, res
 
 //Create a route http://localhost:3000/items
-router.get('/:id', getItem)
+router.get('/:id', logMiddelware, getItem)
 
 router.get('/', getItems)
 
