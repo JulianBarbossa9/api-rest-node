2e9d86a7c4397a9a0afa85d22ef17792e383f2ef
Julian Barbosa <julianbarbossa@hotmail.com>
2023-10-18 20:49:04 -0500

    add and call the function CRUD to our controllers and validate if don't exist data

--- src/controllers/item.ts
+++ src/controllers/item.ts
@@ -1,26 +1,32 @@
 import { Request, Response } from "express"
 import { handleHttp } from "../utils/error.handle"
-import { insertItem } from "../service/item"
+import { insertCar, getCarItems, getCarDetail, updateCar, deleteCar } from "../service/item"
+import ItemModel from "../models/item"
 
-const getItem = (req: Request, res: Response) => {
+
+const getItem = async ({params : { id }} : Request, res: Response)  => {
   try {
-    
+    const responseItem = await getCarDetail(id)//check in your routes if the name in the route name id exam: router.get('/:id', getItem) if the name is router.get('/:wrt', getItem) I need to provide wrt 
+    const data = responseItem ? responseItem : 'NOT_FOUND'
+    res.send(data)
   } catch (error)  {
     handleHttp(res, 'ERROR_GET_ITEM')
   }
 }
 
-const getItems = (req: Request, res: Response) => {
+const getItems = async (req: Request, res: Response) => {
   try {
-    
+    const responseItems = await getCarItems()
+    res.send(responseItems)
   } catch (error)  {
     handleHttp(res, 'ERROR_GET_ITEMS')
   }
 }
 
-const updateItem = (req: Request, res: Response) => {
+const updateItem = async (req: Request, res: Response) => {
   try {
-    
+    const responseItem = await updateCar(req.params.id, req.body)
+    res.send(responseItem)
   } catch (error)  {
     handleHttp(res, 'ERROR_UPDATE_ITEM')
   }
@@ -28,16 +34,17 @@ const updateItem = (req: Request, res: Response) => {
 
 const postItem = async ({ body }: Request, res: Response) => {
   try {
-    const responseItem = await insertItem(body)
+    const responseItem = await insertCar(body)
     res.send(responseItem)
   } catch (error)  {
     handleHttp(res, 'ERROR_POST_ITEM', error)
   }
 }
 
-const deleteItem = (req: Request, res: Response) => {
+const deleteItem = async ({params: { id }}: Request, res: Response) => {
   try {
-    
+    const deleteItem = await deleteCar(id)
+    res.send(deleteItem)
   } catch (error)  {
     handleHttp(res, 'ERROR_DELETED_ITEM')
   }
